0cfe326468f438058bbde8b0b077bc9fba0fb940
matrus <matrus@student.21-school.ru>
2020-08-04 21:34:48 +1200

    modify ft_strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -2,25 +2,18 @@ section .text
 	global ft_strdup
 	extern malloc
 	extern ft_strlen
+	extern ft_strcpy
 
 ft_strdup:			;	rdi - s
 	push rdi		;	save s
 	call ft_strlen
-	push rax		;	save ft_strlen(s);
 	inc rax
 	mov rdi, rax
 	call malloc		;	rax = new_s
-	pop rcx			;	rcx = ft_strlen(s);
-	pop rdi			;	rdi = s
+	pop rsi			;	rsi = s
 	test rax, rax
 	jz .ret
-	mov byte[rax + rcx], 0x00
-.loop_start:
-	test rcx, rcx
-	jz .ret
-	dec rcx
-	mov dl, byte[rdi + rcx]
-	mov byte[rax + rcx], dl
-	jmp .loop_start
+	mov rdi, rax
+	call ft_strcpy
 .ret:
-	ret	
+	ret
